aa5e88c9a45bca6c16821b9a18ba0f3c02418ca1
ivalencius <ilanvalencius@gmail.com>
2022-04-26 00:57:27 -0400

    added one plot functionality

--- make_plots.py
+++ make_plots.py
@@ -12,39 +12,61 @@ from tqdm import tqdm
 import glob
 import matplotlib.pyplot as plt
 
-read_folder = 'C:\\ConwaysTexture\\p_dependency\\'
-plot_name = 'Probability Stats vs. Run #'
+one_plot = True
 
-sort_lam = lambda x : float(os.path.basename(x).strip('.csv'))
-file_list = sorted(glob.glob(os.path.join(read_folder, '*.csv')), key=sort_lam)
-
-# Subplots are organized in a Rows x Cols Grid
-# Tot and Cols are known
-Tot = 8 # number_of_subplots
-Cols = 3# number_of_columns
-
-# Compute Rows required
-Rows = Tot // Cols 
-Rows += Tot % Cols
-
-# Create a Position index
-Position = range(1,Tot + 1)
-
-fig = plt.figure(1)
-fig.subplots_adjust(hspace=.5)
-for file, k in zip(file_list, range(Tot)):
-    basename = os.path.basename(file).strip('.csv')
+if one_plot:
+    # For a single plot
+    file = 'C:\\ConwaysTexture\\trial_dependency\\100.csv'
+    plot_name = 'Stats for 100 trials'
+    #basename = os.path.basename(file).strip('.csv')
     df = pd.read_csv(file)
     x_coords = df['Run']
     df_cols = list(df)
     df_cols.pop(0) # get rid of Run #
-    ax = fig.add_subplot(Rows,Cols,Position[k])
     for col in df_cols:
         #plt.plot(df[col], label=col)
-        ax.plot(df[col], label=col)
-        handles, labels = ax.get_legend_handles_labels()
-        ax.set_title(basename)
-fig.legend(handles, labels, loc='right')
-fig.suptitle(plot_name)
-plt.show()
+        plt.plot(df[col], label=col)
+    plt.xlabel('Run #')
+    plt.ylabel('Metric Value')
+    plt.title(plot_name)
+    plt.legend(loc='right')
+    plt.show()
     
+else:
+    # For Multiple Plots
+    read_folder = 'C:\\ConwaysTexture\\trial_dependency\\'
+    plot_name = 'Trial Stats vs. Run #'
+
+    sort_lam = lambda x : float(os.path.basename(x).strip('.csv'))
+    file_list = sorted(glob.glob(os.path.join(read_folder, '*.csv')), key=sort_lam)
+
+    # Subplots are organized in a Rows x Cols Grid
+    # Tot and Cols are known
+    Tot = 4 # number_of_subplots
+    Cols = 2# number_of_columns
+
+    # Compute Rows required
+    Rows = Tot // Cols 
+    Rows += Tot % Cols
+
+    # Create a Position index
+    Position = range(1,Tot + 1)
+
+    fig = plt.figure(1)
+    fig.subplots_adjust(hspace=.5)
+    for file, k in zip(file_list, range(Tot)):
+        basename = os.path.basename(file).strip('.csv')
+        df = pd.read_csv(file)
+        x_coords = df['Run']
+        df_cols = list(df)
+        df_cols.pop(0) # get rid of Run #
+        ax = fig.add_subplot(Rows,Cols,Position[k])
+        for col in df_cols:
+            #plt.plot(df[col], label=col)
+            ax.plot(df[col], label=col)
+            handles, labels = ax.get_legend_handles_labels()
+            ax.set_title(basename)
+    fig.legend(handles, labels, loc='right')
+    fig.suptitle(plot_name)
+    plt.show()
+        
