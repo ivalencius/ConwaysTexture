70a3fc59e054902b041c2df6a96c1f562f9aef45
ivalencius <ilanvalencius@gmail.com>
2022-05-01 22:30:55 -0400

    added new query

--- query.py
+++ query.py
@@ -1,14 +1,18 @@
+from unittest import result
 import pandas as pd
 import sqlite3
-import glob
-import os
+
 #read_folder = 'C:\\ConwaysTexture\\'
 table_name = '1600x1600_stats.db'
 conn = sqlite3.connect(table_name)
 c = conn.cursor()
-query = 'SELECT * FROM stats WHERE Skewness>0.2 AND Variance>0.005 AND Entropy>0.5 AND Mean>64 AND Mean<192'
+query = 'SELECT * FROM stats WHERE PScore>0.05 AND RUN>0 AND Skewness<0.05 AND Entropy>0.05'
 c.execute(query)
 conn.commit()
-
+results_list = []
 for row in c:
-    print(row)
+    #print(row)
+    results_list.append(row)
+results = pd.DataFrame(results_list)
+results.columns = ['Run', 'Mean','PScore','Variance','Skewness','Kurtosises','Entropy','Contrast','Homogeneity','Probability']
+print(results)
